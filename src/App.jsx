6238a833e3b6ac19c5c43d011abbc37c97a375ca
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-02 21:16:37 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react"
+import React from 'react'
 
-const Card =({title}) =>{
-
-const[count,setCount]=useState(0);
-
-    //is a React state declaration using the useState hook
-    //When clicked, setHasLiked updates the state to true, changing the button text to "Unlike"
-    const[hasLiked,setHasLiked] = useState(false);
-
-
-         //used to check activity on console 37:57
-    useEffect(()=>{console.log(`${title} has been liked : ${hasLiked}`)},[hasLiked]);
-
-
-    return(
-
-        <div  className="card"  onClick={()=>setCount(count+1)} >
-            <h2>{title}<br/>{count||null}</h2>
-
-
-<button onClick={ ()=>setHasLiked(!hasLiked)}>
-    {hasLiked ? "❤️": "🤍"}
-</button>
-
-        </div>
-    )
-
-}
-
-
-const App =() =>{
-
-    
-return(
-
-    <div className="card-container">
-
-<Card title="Star Wars"/>
-<Card title="Avatar"/>
-<Card title="The Lion king"/>
-    </div>
-
-
-)
-
+const App = () => {
+  return (
+    <div> 
+         <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </div>
+  )
 }
 
 export default App
