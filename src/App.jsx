29a35532d86892cb028df9d4952fa0f95c2d6f66
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-14 22:41:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,11 @@ Find <span className='text-gradient'>Movies </span>You'll Enjoy Without the Hass
   
  <section className ="all-movies">
 <h2>All Movies</h2>
-{errorMessage && <p className="text-red-500">{errorMessage}</p>}
+{isLoading ?(
+<p className='text-white'>Loading......</p>
+
+)
+}
  </section>
 
 </div>
