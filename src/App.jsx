83d58e00c35d0a347b3369fbe9ba3836a0b828d8
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-15 23:47:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,9 @@ Find <span className='text-gradient'>Movies </span>You'll Enjoy Without the Hass
   <p  className='text-red'>{errorMessage}</p>
 ) : (
   <ul>
-   }
+    {movieList.map((movie)=>
+
+ )) }
   </ul>
 )
 }
