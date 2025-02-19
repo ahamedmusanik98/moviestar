87fc7427e443c43e8b3a2034f8d4d48221367203
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-19 22:44:51 +0530

    update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const MovieCard = ({movie:{title}}) => {
+const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_lanuguage_}
+}) => {
   return (
     <div>
         <p className='text-white'>{title}</p>
