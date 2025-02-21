c8fd10268297bb32bb5746e53a33f65809255cbc
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-21 22:47:09 +0530

    update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_
 }) => {
   return (
     <div className='movie-card'>
-      <img  src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'no-movie.png'}
+      <img  src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
       alt={title}
       />
 <div className='mt-4'>
