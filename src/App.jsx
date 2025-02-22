e0dc748e4748c66244254d4e08e33b311075e4bc
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-22 22:59:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { use } from 'react';
 import MovieCard from './components/MovieCard';
 
 
-const API_BASE_URL='https://api.themoviedb.org/3';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
 
@@ -13,18 +13,19 @@ const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS ={
   method:'GET',
   headers: {
-    accepet :'application/json',
+    accept :'application/json',
     Authorization :`Bearer ${API_KEY}`,
   },
 }
 
 const App = () => {
 
-  const [searchTerm, setserachTerm] = useState('');
+  const [searchTerm, setSerachTerm] = useState('');
 
-  const [errorMessage, setErrorMessage] = useState('');
 
   const [movieList,setMovieList] =useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
 
   const [isLoading,setIsLoading] =useState(false);
   
@@ -83,7 +84,7 @@ Find <span className='text-gradient'>Movies </span>You'll Enjoy Without the Hass
  <section className ="all-movies">
 <h2 className='mt-[40px]'>All Movies</h2>
 {isLoading ?(
-  <Spinner/>
+  <spinner/>
 
 ) : errorMessage ?(
   <p  className='text-red'>{errorMessage}</p>
