83cca3a08db20eae24ad90c65cf635a45a91bab0
Ahamed Musanik <139645056+ahamedmusanik98@users.noreply.github.com>
2025-02-22 23:06:36 +0530

    update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_lanuguage_}
+const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_lanuguage}
 }) => {
   return (
     <div className='movie-card'>
@@ -10,19 +10,20 @@ const MovieCard = ({movie:{title,vote_average,poster_path,release_date,original_
 <div className='mt-4'>
 <h3> {title}  </h3>
 
-<div className='content'></div>
+<div className='content'>
 <div className='rating'>
 <img src='star.svg' alt='Star Icon'/>
 <p>{vote_average ? vote_average.toFixed(1):'N/A'}</p>
 </div>
 
 <span>•</span>
-<p className='lang'></p>
+<p className='lang'>{original_lanuguage}</p>
+
 <span>•</span>
 <p className='year'></p>
 {release_date ? release_date.split('-'): 'N/A'}
 </div>
-
+</div>
     </div>
   )
 }
